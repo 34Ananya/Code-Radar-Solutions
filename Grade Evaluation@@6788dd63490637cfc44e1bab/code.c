52cd81dd1c051b03b3ad3eb335e605c073d67495
Ananya Malhotra <ananya.malhotra2019@gmail.com>
2025-02-13 17:26:58 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -5,16 +5,22 @@ int main(){
     switch(c){
         case 'A':
         printf("Excellent");
+        break;
         case 'B':
         printf("Good");
+        break;
         case 'C':
         printf("Average");
+        break;
         case 'D':
         printf("Below Average");
+        break;
         case 'E':
         printf("Fail");
+        break;
         default:
         printf("Invalid grade");
+        break;
     }
     return 0;
 }
